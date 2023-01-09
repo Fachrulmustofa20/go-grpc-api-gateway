91ebd5701182754fb7f6d8c4a90fc82e8cbb5c63
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2023-01-09 13:36:59 +0700

    add client order

---
+++ pkg/order/client.go
@@ -0,0 +1,22 @@
+package order
+
+import (
+	"fmt"
+
+	"github.com/Fachrulmustofa20/go-grpc-api-gateway/pkg/config"
+	"github.com/Fachrulmustofa20/go-grpc-api-gateway/pkg/order/pb"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.OrderServiceClient
+}
+
+func InitServiceClient(cfg *config.Config) pb.OrderServiceClient {
+	cc, err := grpc.Dial(cfg.OrderSvcUrl, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Could not connect: ", err)
+	}
+
+	return pb.NewOrderServiceClient(cc)
+}
